962b26ed74fcdec45fd5648bcdea5d675e33bdd3
matt-schaller <mschaller3@gmail.com>
2020-07-27 09:50:53 -0400

    Revert

--- src/app.js
+++ src/app.js
@@ -1,13 +1,17 @@
 const { getBase64DataURI } = require('dauria');
 const path = require('path');
+
+
+// // 🤞 Declaring NODE_CONFIG_DIR before @feathersjs/configuration loads.
+// process.env['NODE_CONFIG_DIR'] = path.join(__dirname, 'config/')
+
+
 const favicon = require('serve-favicon');
 const compress = require('compression');
 const helmet = require('helmet');
 const cors = require('cors');
 const logger = require('./logger');
 
-// 🤞 Declaring NODE_CONFIG_DIR before @feathersjs/configuration loads.
-process.env['NODE_CONFIG_DIR'] = path.join(__dirname, 'config/')
 
 const feathers = require('@feathersjs/feathers');
 const configuration = require('@feathersjs/configuration');
